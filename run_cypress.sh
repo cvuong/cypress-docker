26bd5c39ade59c5e375508e631ce9a0ff2faae73
Charlie Vuong <charlievuong@pinterest.com>
2019-10-20 21:36:42 -0700

    Uncomment installing and running cypress

--- run_cypress.sh
+++ run_cypress.sh
@@ -2,10 +2,11 @@
 echo "Buildkite ENV"
 echo $BUILDKITE_PARALLEL_JOB
 echo $BUILDKITE_PARALLEL_JOB_COUNT
+
 # We are using npm to install cypress packages temporarily
 # Ideally, we should use the cypress/included image dependency
 # in the docker-compose.yml file so we don't have to install
 # cypress using npm with each run.
-#npm install
-#$(npm bin)/cypress install
-#$(npm bin)/cypress run
+npm install
+$(npm bin)/cypress install
+$(npm bin)/cypress run
