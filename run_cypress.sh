bb7f0a49b5e15e70cc7ee502a0e8c83773347b5e
Charlie Vuong <charlievuong@pinterest.com>
2019-10-21 13:54:16 -0700

    Add parallelization to run_cypress script

--- run_cypress.sh
+++ run_cypress.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 echo "Buildkite ENV"
-echo $BUILDKITE_PARALLEL_JOB
-echo $BUILDKITE_PARALLEL_JOB_COUNT
+echo "BUILDKITE_PARALLEL_JOB: $BUILDKITE_PARALLEL_JOB"
+echo "BUILDKITE_PARALLEL_JOB_COUNT: $BUILDKITE_PARALLEL_JOB_COUNT"
+printf "\n\n"
 
-# We are using npm to install cypress packages temporarily
-# Ideally, we should use the cypress/included image dependency
-# in the docker-compose.yml file so we don't have to install
-# cypress using npm with each run.
+cypress_tests=($(find cypress/integration -type f -name "*.js" | sort -V))
+job_tests=()
+for i in "${!cypress_tests[@]}"; do
+    remainder=$((($i + $BUILDKITE_PARALLEL_JOB) % $BUILDKITE_PARALLEL_JOB_COUNT))
+    if [ $remainder -eq 0 ]; then
+      job_tests+=(${cypress_tests[$i]})
+    fi
+done
+
+cypress_spec=$(IFS=,; echo "\"${job_tests[*]}\"")
 npm install
 $(npm bin)/cypress install
-$(npm bin)/cypress run
+command="$(npm bin)/cypress run --spec $cypress_spec"
+# Is this safe?
+eval $command
